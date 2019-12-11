3c8d8d05f7c852235fcc2edcbd270026b7d7bade
Patricia Costalonga <50948965+Nuity29@users.noreply.github.com>
2019-12-11 14:05:07 +0000

    JS function working

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
-// First challenge:
-//
-// var button = document.getElementsByTagName("button")[0];
-//
-// button.addEventListener("click", function()
-// {console.log("CLICK!!!");
-// })
-//
-// Second challenge even more better written cash queries:
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var checkedItem = document.getElementById("list");
 
-function inputLength(){
+function inputLength() {
   return input.value.length;
 }
 
-function createListElement(){
+function createListElement() {
   var li = document.createElement("li");
   var extra = input.value;
-  li.appendChild(document.createTextNode(input.value));
+  li.appendChild(document.createTextNode(extra));
   ul.appendChild(li);
   li.innerHTML = extra + "<button id='remove'>X</button>";
   input.value = "";
 }
 
-function addListAfterClick(){
-  if (inputLength() > 0){
+function addListAfterClick() {
+  if (inputLength() > 0) {
     createListElement();
   }
 }
 
-function addListAfterKeyPress(event){ //this function needs a parameter to talk to event.keyCode below
-  if (inputLength() > 0 && event.keyCode === 13){
+function addListAfterKeyPress(event) { //this function needs a parameter to talk to event.keyCode below
+  if (inputLength() > 0 && event.keyCode === 13) {
     createListElement();
   }
 }
